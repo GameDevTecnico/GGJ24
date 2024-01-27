d5fc62b049e9659a15236812e3674fa9ab7a11c5
Andre Correia <91163660+andremcorreia@users.noreply.github.com>
2024-01-27 17:53:29 +0000

    lvl1 kinda done

--- Assets/Scripts/CharCreator.cs
+++ Assets/Scripts/CharCreator.cs
@@ -4,15 +4,41 @@ using UnityEngine;
 
 public class CharCreator : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public int head = -1;
+    public int leg = -1;
+    public int torso = -1;
+    [SerializeField] private List<Sprite> HeadParts = new List<Sprite>();
+    [SerializeField] private GameObject headGameObject;
+    [SerializeField] private List<Sprite> LegParts = new List<Sprite>();
+    [SerializeField] private GameObject legGameObject;
+    [SerializeField] private List<Sprite> TorsoParts = new List<Sprite>();
+    [SerializeField] private GameObject torsoGameObject;
 
-    // Update is called once per frame
+
     void Update()
     {
-        
+        if (head >= 0)  
+        {
+            headGameObject.GetComponent<SpriteRenderer>().sprite = HeadParts[head];
+        }
+        if (leg >= 0)
+        {
+            legGameObject.GetComponent<SpriteRenderer>().sprite = LegParts[head];
+        }
+        if (torso >= 0)
+        {
+            torsoGameObject.GetComponent<SpriteRenderer>().sprite = TorsoParts[head];
+        }
     }
+
+    public void setLegs(int x){
+        leg = x;
+    }
+    public void setHead(int x){
+        head = x;
+    }
+    public void setTorso(int x){
+        torso = x;
+    }
+
 }
