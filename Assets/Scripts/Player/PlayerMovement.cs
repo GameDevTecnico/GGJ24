e597cac4e76a44e56af3f1c1ff7f952b840ff824
xhmueel <105432909+xhmueel@users.noreply.github.com>
2024-01-27 22:11:54 +0000

    Implement level 3
     - Player Death
     - Tickle Mechanic (Incomplete)

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.InteropServices.WindowsRuntime;
 using Unity.VisualScripting;
 using UnityEngine;
 
@@ -10,9 +11,12 @@ public class PlayerMovement : MonoBehaviour
     private BoxCollider2D coll;
 
     [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask footLayer;
 
     [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 5f;
+
+    [SerializeField] private float footDistance;
     // Start is called before the first frame update
     void Start()
     {
@@ -37,5 +41,11 @@ public class PlayerMovement : MonoBehaviour
     private bool IsGrounded()
     {
         return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, groundLayer);
+    }
+
+    public bool IsNearFoot()
+    {
+        print("aaaa");
+        return Physics2D.Raycast(transform.position, Vector2.right, footDistance, footLayer) || Physics2D.Raycast(transform.position, Vector2.left, footDistance, footLayer);
     }
 }
