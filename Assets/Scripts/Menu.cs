6178fa3f598af27bf7203ee22ad7d324a712c7a7
Andre Correia <91163660+andremcorreia@users.noreply.github.com>
2024-01-28 14:53:51 +0000

    Menu

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -6,11 +6,18 @@ using UnityEngine.SceneManagement;
 public class Menu : MonoBehaviour
 {
     public AudioClip[] audioClips;
+    public AudioClip[] audioClipsStart;
     private AudioSource audioSource;
     private int audioIndex = 0;
 
+    private bool playing = false;
+
     private void Start()
     {
+        for (int i = 0; i < 5; i++)
+        {
+            StartCoroutine(PlayAudio(i));
+        }
         audioSource = GetComponent<AudioSource>();
     }
 
@@ -35,7 +42,25 @@ public class Menu : MonoBehaviour
 
     IEnumerator DelayedPlayButton()
     {
-        yield return new WaitForSeconds(audioSource.clip.length);
+        yield return new WaitForSeconds(3);
         PlayButton();
+    }
+
+    private IEnumerator PlayAudio(int i)
+    {
+        while (GetComponent<AudioSource>().isPlaying || playing)
+        {
+            yield return null;
+        }
+
+        GetComponent<AudioSource>().clip = audioClipsStart[i];
+        GetComponent<AudioSource>().Play();
+
+        playing = true;
+
+        yield return new WaitForSeconds(audioClipsStart[i].length);
+        yield return new WaitForSeconds(2.5f);
+
+        playing = false;
     }
 }
